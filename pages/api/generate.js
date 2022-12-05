edad82c61f18cd82e927cee5a4b456a426fddc12
Arif Yulianto <arif.yulianto@peruri.co.id>
2022-12-05 15:34:02 +0700

    latest build

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -19,8 +19,8 @@ const generateAction = async (req, res) => {
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.8,
-    max_tokens: 250,
+    temperature: 0.7,
+    max_tokens: 1500,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
